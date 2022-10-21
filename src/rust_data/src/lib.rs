f59e4eea3b628f61b64c0d33fe134b7a1cdbbe31
David Dal Busco <david.dalbusco@outlook.com>
2022-10-21 15:05:06 +0200

    feat: post upgrade decode candid

--- src/rust_data/src/lib.rs
+++ src/rust_data/src/lib.rs
@@ -1,11 +1,11 @@
 mod types;
 
 use crate::types::{ Assets, State, Asset };
-use ic_cdk::{ storage, print };
+use ic_cdk::{ storage, print, api };
 use ic_cdk_macros::{ init, query, update, pre_upgrade, post_upgrade };
 use std::cell::RefCell;
 use std::collections::HashMap;
-use candid::CandidType;
+use candid::{CandidType, decode_args};
 use serde::Deserialize;
 
 thread_local! {
@@ -25,8 +25,13 @@ fn init(user: String) {
 
 #[derive(CandidType, Deserialize)]
 struct UpgradeState {
-    pub user: String,
-    pub entries: Vec<(String, Asset)>,
+    pub user: Option<UpgradeUser>,
+    pub entries: Option<Vec<(String, Asset)>>,
+}
+
+#[derive(CandidType, Deserialize)]
+struct UpgradeUser {
+    user: String,
 }
 
 #[pre_upgrade]
@@ -34,21 +39,25 @@ fn pre_upgrade() {
     // TODO: Is there another a simpler way to save the all state?
     // STATE.with(|state| storage::stable_save((&state.borrow(),)).unwrap()); -> error
 
-    let owner: String = STATE.with(|state| get_owner(&state.borrow()));
-    let assets: Assets = STATE.with(|state| get_assets(&state.borrow()));
+    // let owner: String = STATE.with(|state| get_owner(&state.borrow()));
+    // let assets: Assets = STATE.with(|state| get_assets(&state.borrow()));
 
-    let entries: Vec<(String, Asset)> = assets.into_iter().collect();
+    // let entries: Vec<(String, Asset)> = assets.into_iter().collect();
 
-    let save: UpgradeState = UpgradeState { user: owner, entries };
+    // let user: UpgradeUser = UpgradeUser {user: owner};
+    // let save: UpgradeState = UpgradeState { user, entries };
 
-    storage::stable_save((&save,)).unwrap()
+    // storage::stable_save((&save,)).unwrap()
 }
 
 #[post_upgrade]
 fn post_upgrade() {
-    // TODO: error Panicked at 'called `Result::unwrap()` on an `Err` value: "Custom(Cannot parse header 
-    // Issue: https://forum.dfinity.org/t/upgrade-canister-from-motoko-to-rust-with-stable-memory/    
-    let (upgrade_state,): (UpgradeState,) = storage::stable_restore().unwrap();
+    // TODO: error Panicked at 'called `Result::unwrap()` on an `Err` value: "Custom(Cannot parse header
+    // Issue: https://forum.dfinity.org/t/upgrade-canister-from-motoko-to-rust-with-stable-memory/
+
+    // Post upgrade from Rust to Rust
+
+    /* let (upgrade_state,): (UpgradeState,) = storage::stable_restore().unwrap();
 
     let assets: Assets = upgrade_state.entries.into_iter().collect();
 
@@ -59,8 +68,39 @@ fn post_upgrade() {
 
     STATE.with(|state| {
         *state.borrow_mut() = new_state;
-    });
+    }); */
+
+    // https://docs.rs/ic-cdk/0.3.2/src/ic_cdk/api/stable.rs.html#64
+    // let bytes: Vec<u8> = api::stable::stable_bytes();
+
+    let mut stable_length_buf = [0u8; std::mem::size_of::<u32>()];
+    ic_cdk::api::stable::stable_read(0, &mut stable_length_buf);
+    let stable_length = u32::from_le_bytes(stable_length_buf); // maybe use from_be_bytes, I don't remember what endianess is candid
+ 
+    let mut buf = vec![0u8; stable_length as usize];
+    ic_cdk::api::stable::stable_read(std::mem::size_of::<u32>() as u32, &mut buf);
+
+    
+    // let size = (api::stable::stable_size() as usize) << 16;
+    // let mut vec = Vec::with_capacity(size);
+    // unsafe {
+    //     vec.set_len(size);
+    // }
+    // api::stable::stable_read(std::mem::size_of::<u32>() as u32, vec.as_mut_slice());
+
+    let (upgrade_state,): (UpgradeState,) = decode_args(&buf).unwrap();
+    
+    
+    // let (user_bytes, entries_bytes) = from_bytes(bytes).map(|c| c.0)?;
+    // let user: String = Decode!(&user_bytes, {user: String}).map_err(|err| err.to_string());
+    // let entries: Vec<(String, Asset)> = Decode!(&user_bytes, Vec<(String, Asset)>).map_err(|err| err.to_string());
 }
+
+// From dfn_candid https://github.com/dfinity/ic/blob/89446f5a04f053040b4863eab5458446d925ed0e/rs/rust_canisters/dfn_candid/src/lib.rs
+// fn from_bytes(bytes: Vec<u8>) -> Result<(Vec<u8>, Vec<u8>), String> {
+//    let res = decode_args(&bytes).map_err(|e| e.to_string())?;
+//    Ok(res)
+// }
 
 #[query]
 fn get() -> State {
